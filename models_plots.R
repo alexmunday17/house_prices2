018eb769b89e1dbc933b851236bc8b2c15792432
alexmunday17 <alexmunday@live.co.uk>
2022-12-23 17:24:47 +0000

    progress

--- models_plots.R
+++ models_plots.R
@@ -3,10 +3,31 @@ library(ggplot2)
 library(mgcv)
 
 houses <- readRDS("data/houses_london_clean.Rds")
+
+setkey(houses, date, price)
+fake_data <- data.table(date = houses[, seq(min(date), max(date), by = 1)],
+                        N = 0)
+houses_count <- rbind(fake_data, houses[, .N, by = date])
+setkey(houses_count, date, N)
+houses_count <- houses_count[!duplicated(date, fromLast = TRUE)]
+houses_count[, rolling_n := frollsum(N, 30)]
+ggplot(houses_count) + geom_line(aes(x = date, y = rolling_n))
+
+houses[, price_cap := quantile(price, 0.4), by = year(date)]
+houses_count <- rbind(fake_data, houses[price < price_cap, .N, by = date])
+setkey(houses_count, date, N)
+houses_count <- houses_count[date < "2022-07-01"]
+houses_count <- houses_count[!duplicated(date, fromLast = TRUE)]
+houses_count[, rolling_n := frollsum(N, 30)]
+ggplot(houses_count) + geom_line(aes(x = date, y = rolling_n))
+
+
+# smooth on time of year
+# just look at houses < xth quantile
+
 houses <- houses[between(price, quantile(price, 0.025), quantile(price, 0.975))]
 
 ggplot(houses) + geom_density(aes(x = price))
-
 ggplot(houses) + geom_smooth(aes(x = date, y = price))
 ggplot(houses) + geom_smooth(aes(x = yday(date), y = price))
 
